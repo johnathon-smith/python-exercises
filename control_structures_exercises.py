14e04e3cb0c788de1a4a755e93e93efede6b6524
Johnathon Smith <smith.johnathon01@gmail.com>
2021-07-11 15:37:22 -0500

    Completed control_structures_exercises.py

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -91,3 +91,182 @@ print("\n")
 #Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 #(Hint: use the isdigit method on strings to determine this). Use a loop and the continue statement to output all the odd numbers between 1 and 50,
 #except for the number the user entered.
+users_number = input('Please enter an odd number between 1 and 50: ')
+
+while True:
+    if users_number.isdigit() == True and int(users_number) % 2 == 1 and int(users_number) >= 1 and int(users_number) <= 50:
+        break
+    print('\nInvalid input')
+    users_number = input('Please enter an odd number between 1 and 50: ')
+
+print('\n')
+print('Number to skip:', users_number)
+
+for i in range(1, 51, 2):
+    if i == int(users_number):
+        print('This number skipped!')
+        continue
+    print('Here is an odd number:', i)
+
+print('\n')
+
+#The input function can be used to prompt for input and use that input in your python code. 
+#Prompt the user to enter a positive number and write a loop that counts from 0 to that number. 
+#(Hints: first make sure that the value the user entered is a valid number, also note that the input function returns a string, 
+#so you'll need to convert this to a numeric type.)
+users_number = input('Please enter any positive number: ')
+
+while users_number.isdigit() != True or int(users_number) <= 0:
+    print('\nInvalid input')
+    users_number = input('Please enter any positive number: ')
+
+print('\n')
+print('Counting to', users_number)
+for i in range(0, int(users_number) + 1):
+    print(i)
+
+print('\n')
+
+#Write a program that prompts the user for a positive integer. Next write a loop that prints out the numbers from the number the user entered down to 1.
+users_number = input('Please enter any positive number: ')
+
+while users_number.isdigit() != True or int(users_number) <= 0:
+    print('\nInvalid input')
+    users_number = input('Please enter any positive number: ')
+
+print('\n')
+print('Counting down from:', users_number)
+i = int(users_number)
+while i >= 1:
+    print(i)
+    i -= 1
+
+print('\n')
+
+#FizzBuzz
+#Write a program that prints the numbers from 1 to 100.
+#For multiples of three print "Fizz" instead of the number
+#For the multiples of five print "Buzz".
+#For numbers which are multiples of both three and five print "FizzBuzz".
+print('FizzBuzz Test:')
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+
+print('\n')
+
+#Display a Table of Powers
+#Prompt the user to enter an integer.
+#Display a table of squares and cubes from 1 to the value entered.
+#Ask if the user wants to continue. I'm assuming this means if the user wants to pick another number and do this again.
+#Assume that the user will enter valid data.
+#Only continue if the user agrees to.
+
+while True:
+    users_number = input('What number would you like to go up to? ')
+    print('\n')
+
+    print('Here is your table!')
+    print('\n')
+    print('Number | Squared | Cubed ')
+    print('------ | ------- | -----')
+
+    for i in range(1, int(users_number) + 1):
+        print(f'{i: <6} | {pow(i, 2): <7} | {pow(i,3): <}')
+
+    print('\n')
+
+    wants_to_continue = input('Do you want to continue? Y for yes, N for no: ')
+    print('\n')
+
+    if wants_to_continue[0].lower() == 'y':
+        continue
+    else:
+        break
+
+
+#Convert given number grades into letter grades.
+
+"""
+Prompt the user for a numerical grade from 0 to 100.
+Display the corresponding letter grade.
+Prompt the user to continue.
+Assume that the user will enter valid integers for the grades.
+The application should only continue if the user agrees to.
+Grade Ranges:
+
+A : 100 - 88
+B : 87 - 80
+C : 79 - 67
+D : 66 - 60
+F : 59 - 0
+"""
+while True:
+    users_grade = int(input('Please enter a numerical grade from 0 to 100: '))
+    print('\n')
+
+    if users_grade < 60:
+        print('Your letter grade is: F')
+    elif users_grade < 67:
+        print('Your letter grade is: D')
+    elif users_grade < 80:
+        print('Your letter grade is: C')
+    elif users_grade < 88:
+        print('Your letter grade is: B')
+    else:
+        print('Your letter grade is: A')
+    
+    print('\n')
+    wants_to_continue = input('Would you like to continue? Y for yes, N for no: ')
+    print('\n')
+
+    if wants_to_continue[0].lower() == 'y':
+        continue
+    else:
+        break
+
+
+#Create a list of dictionaries where each dictionary represents a book that you have read. Each dictionary in the list should have the keys title, author,
+#and genre. Loop through the list and print out information about each book.
+
+#Prompt the user to enter a genre, then loop through your books list and print out the titles of all the books in that genre.
+books = [
+    {
+        'Title': 'Eragon',
+        'Author': 'Christopher Paolini',
+        'Genre': 'Fantasy'
+    },
+    {
+        'Title': 'The Outsider',
+        'Author': 'Stephen King',
+        'Genre': 'Horror'
+    },
+    {
+        'Title': 'Unscripted',
+        'Author': 'James DeMarco',
+        'Genre': 'Business'
+    },
+    {
+        'Title': 'Mr. Mercedes',
+        'Author': 'Stephen King',
+        'Genre': 'Horror'
+    }
+]
+
+print("Books I have read:")
+for book in books:
+    print(f"{book['Title']}, by {book['Author']}. Genre: {book['Genre']}")
+
+print('\n')
+chosen_genre = input("Please enter a genre to search for: ")
+
+print('Books in your genre:')
+for book in books:
+    if book['Genre'] == chosen_genre:
+        print(f"{book['Title']}")
